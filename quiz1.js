09c2880b22f22f125b3fdd3b4ec7e2a98a1bcf49
STARZ333 <53653360+STARZ333@users.noreply.github.com>
2021-03-29 01:43:10 +0800

    Add files via upload

--- quiz1.js
+++ quiz1.js
@@ -1,12 +1,19 @@
 function change() 
-{     var header = document.getElementById("cgu"); 
+{     $("#cgu").html("CSIE@CGU");
+      $("#bb").html("怎麼那麼棒！！");
+      /*var header = document.getElementById("cgu"); 
       header.innerHTML = "CSIE@CGU";    
-      var para = document.getElementById("bb");  
-      para.innerHTML = "怎麼那麼棒！！";   
+      var $para = $("#bb")[0];  
+      para.innerHTML = "怎麼那麼棒！！";   */
             }  
 function newbutton(){
-      var btn = document.createElement("BUTTON"); 
+      var change_button="<button id='change'>Change this document</button>";
+      $("body").append(change_button);
+      $("#change").click(change);
+      /*var btn = document.createElement("BUTTON"); 
       btn.innerHTML = "Change this document";
       document.body.appendChild(btn); 
       btn.addEventListener("click",change);
-    }
+      */
+}
+$("#btn").click(newbutton);
